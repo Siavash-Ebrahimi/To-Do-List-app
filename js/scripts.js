19380213a808c01227c0df637d7e5a13874218bb
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-09-11 06:18:39 +0400

    Used jQuery on .js file to create the app.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+
+function newItem(){
+  // Click on button at HTML:
+  let inputValue = $('#input').val();
+  let li = $('<li></li>');
+  li.append(inputValue);
+
+  // Check if the input box is empty or not:
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+  //
+  li.on('dblclick', function(event) {
+		li.toggleClass("strike");
+	});
+  // 3. Adding a delete button
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+  function deleteListItem(){
+		li.addClass('delete')
+	}
+   $('#list').sortable();
+}
