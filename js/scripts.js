cc766e6ebdf35361160d68f02e04437c5705bce6
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-09-11 06:40:34 +0400

    Updated comments on main code parts

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
-
+// Click on button at HTML:
 function newItem(){
-  // Click on button at HTML:
+
   let inputValue = $('#input').val();
   let li = $('<li></li>');
   li.append(inputValue);
 
-  // Check if the input box is empty or not:
+  // A) Check if the input box is empty or not:
   if (inputValue === '') {
     alert("You must write something!");
   } else {
     $('#list').append(li);
   }
-  //
+
+  // B) Added double click functionality to each created <li>
+  // for making a strike (_____) line on item in the <li>.
   li.on('dblclick', function(event) {
 		li.toggleClass("strike");
 	});
-  // 3. Adding a delete button
+
+  // C) Added a button shape of X (as a text) with click ability to each created <li>
   let crossOutButton = $('<crossOutButton></crossOutButton>');
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
-
   crossOutButton.on('click', function(event){
     li.addClass('delete')
   });
-  
+
+  // D) Using jQuery .sortable() function for <ol id="list"></ol> in HTML
+  // to sortable all items inside -> $('#list') by drag and drop.
    $('#list').sortable();
 }
